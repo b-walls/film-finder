14df7390de77c8c0590064f950fae06367acb6c9
Brendan <brendan.walls22@gmail.com>
2025-04-18 17:13:19 -0400

    Add target="_blank" to TMDB attribution links for improved user experience

--- frontend/src/components/TMDBAttributionIcon.jsx
+++ frontend/src/components/TMDBAttributionIcon.jsx
@@ -4,11 +4,11 @@ import { Link, Image, Text, VStack} from '@chakra-ui/react'
 const TMDBAttributionIcon = () => {
   return (
     <VStack align="center">
-      <Link href="https://www.themoviedb.org/">
+      <Link href="https://www.themoviedb.org/" target="_blank">
         <Image src={"https://www.themoviedb.org/assets/2/v4/logos/v2/blue_short-8e7b30f73a4020692ccca9c88bafe5dcb6f8a62a4c6bc55cd9ba82bb2cd95f6c.svg"} alt="Data provided by The Movie Database" w="100px" objectFit={"contain"}/>
       </Link>
       <Text fontSize="xs" color="fg.muted" mb={4}>
-        Provided by <Link  href="https://www.themoviedb.org/">themoviedb.org</Link>
+        Provided by <Link  href="https://www.themoviedb.org/" target="_blank">themoviedb.org</Link>
       </Text>
     </VStack>
   )
